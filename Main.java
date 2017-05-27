5ce6795dbb19551f7c01390a41446b885e5cb352
Gino Gustavo <ing.gino@gmail.com>
2017-05-27 07:29:58 -0500

    Message changed in java file

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-    for (int i=0;i<=10;i++) {
-        System.out.println("Hello World" + i);
+    for (int i=0;i<=5;i++) {
+        System.out.println("Jenkins Process " + i);
     }
     }
 }
